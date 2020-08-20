a4e0e9848a0f11ffa10793cbaf7c558f0ba88ad0
DSX <debmalya.desarkar@gmail.com>
2020-08-20 20:23:37 +0530

    Updates

---
+++ precache-manifest.1794db3a27587bed30104aaf9e3e85db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86942657f1ddaa82514b457cc7899d69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64544c498fff1e0d1c91",
+    "url": "/static/css/main.1218002f.chunk.css"
+  },
+  {
+    "revision": "1a2541eda9df5df7b172",
+    "url": "/static/js/2.4c6324d6.chunk.js"
+  },
+  {
+    "revision": "bdcfdbef37b771299e3e4252a0d10375",
+    "url": "/static/js/2.4c6324d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64544c498fff1e0d1c91",
+    "url": "/static/js/main.e791e92f.chunk.js"
+  },
+  {
+    "revision": "79c19766b9fafc23cac3",
+    "url": "/static/js/runtime-main.e387f811.js"
+  }
+]);
