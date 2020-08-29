81fcec37ac124bcfaffe201644db1444babd4511
DSX <debmalya.desarkar@gmail.com>
2020-08-29 19:41:31 +0530

    Updates

--- precache-manifest.6b306c610d9916a8b8ed558a3ee64156.js
+++ precache-manifest.6b306c610d9916a8b8ed558a3ee64156.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "72dea5979c9479247fd0a4c663dc3f31",
+    "revision": "954211d88e94f123ad0e0b7bf9240720",
     "url": "/index.html"
   },
   {
-    "revision": "8db75728fe5d3cbee8b2",
-    "url": "/static/css/main.a153d4b1.chunk.css"
+    "revision": "a40628e58d0f95a6ef7a",
+    "url": "/static/css/main.c53667a2.chunk.css"
   },
   {
     "revision": "bb776326f344b0a29dcb",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.34cceccb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8db75728fe5d3cbee8b2",
-    "url": "/static/js/main.73dfe032.chunk.js"
+    "revision": "a40628e58d0f95a6ef7a",
+    "url": "/static/js/main.b82223da.chunk.js"
   },
   {
     "revision": "79c19766b9fafc23cac3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4cf1b28a225b091f24c3ac6e078c332.js"
+  "/precache-manifest.6b306c610d9916a8b8ed558a3ee64156.js"
 );
 
 self.addEventListener('message', (event) => {
